888fb29aaf5905ca3ea1007e8a049fc1a6263a82
arshadahamed <arshadpayoneer@gmail.com>
2024-09-18 00:00:26 +0530

    Initial Commit

---
+++ EmployeeAdminPortal/Models/AddEmployeeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeAdminPortal.Models
+{
+    public class AddEmployeeDto
+    {
+        public required string Name { get; set; } /* required = mean Must Enter*/
+        public required string Email { get; set; }
+        public string? Phone { get; set; } /*string? = mean Nullable*/
+        public decimal Salary { get; set; }
+    }
+}
---
+++ EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; } /* required = mean Must Enter*/
+        public required string Email { get; set; }
+        public string? Phone { get; set; } /*string? = mean Nullable*/
+        public decimal Salary { get; set; }
+
+    }
+}
